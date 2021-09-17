b39bd57bf5757779cc9304026746a7082415d5e4
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2021-09-16 20:53:32 -0500

    uploading images

--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-	username: String,
-	car: String,
-	price: String
-})
+  username: String,
+  car: String,
+  price: String,
+  image: String,
+});
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- server.js
+++ server.js
@@ -1,50 +1,69 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("./db/db");
-const bodyParser = require('body-parser');
-
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-
-const newUsers = [
-	{name: "Ulukbek"}, 
-	{name: "Jim"} , 
-	{name: "Adinay"}, 
-	{name: "Begimay"}
-]
-
-
+const db = require("./db/db");
+const User = require("./models/User");
+const { findByIdAndUpdate } = require("./models/User");
 
 const allowList = "http://localhost:3000";
-
 const corsOption = {
-	origin: allowList
-}
-
-app.use(express.json());
-
+  origin: allowList,
+};
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors(corsOption));
 
-app.get("/", async(req, res) => {
-	res.json({
-		users: newUsers
-	})
+app.get("/clients", async (req, res) => {
+  const users = await User.find({});
+  res.json({
+    users: users,
+  });
 });
 
-app.post("/new-data", async(req, res, next) => {
-	const { username, car, price} = req.body;
-	console.log(username, car, price);
-	res.json({
-		status: 200,
-		message: "success"
-	})
-})
+app.post("/new-data", async (req, res, next) => {
+  const { username, car, price, image } = req.body;
+  const data = {
+    username: username,
+    car: car,
+    price: price,
+    image: image,
+  };
+  const newUser = await new User(data);
+  newUser.save(async (err, user) => {
+    if (err) return console.log(err);
+    const foundUser = await User.find({});
+    res.json({
+      status: 200,
+      message: foundUser,
+    });
+  });
+});
+
+app.delete("/user/:id", async (req, res) => {
+  await User.findByIdAndDelete(req.params.id, async (err, item) => {
+    if (err) console.log("item is not deleted");
+    else {
+      console.log("item deleted");
+      const users = await User.find({});
+      res.json({
+        status: 200,
+        users: users,
+      });
+    }
+  });
+});
+
+app.put("/update-data/:id", async (req, res) => {
+  console.log(req.body);
+  const user = await User.findByIdAndUpdate(req.params.id, req.body);
+  const users = await User.find({});
+
+  res.json({
+    status: 200,
+    data: users,
+  });
+});
 
 app.listen(9000, () => {
-	console.log("app listen on port 9000")
-})
+  console.log("app listen on port 9000");
+});
